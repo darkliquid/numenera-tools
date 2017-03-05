df14bb796a910cf2162248d5a3d8c07a6b68b007
Andrew Montgomery-Hurrell <darkliquid@darkliquid.co.uk>
2017-03-05 21:25:40 +0000

    fixed typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
         <p>
           I've also worked on a number of published Numenera supplements, which
           you can find on <a href="http://www.drivethrurpg.com/browse.php?x=0&y=0&author=Andrew%20Montgomery-Hurrell&affiliate_id=201046">drivethrurpg</a> if you're interested, the proudest achievement
-          amoung them being <a href="http://www.drivethrurpg.com/product/185316/Rusthaven-An-Iron-Wind-Sourcebook-for-Numenera?affiliate_id=201046">Rusthaven</a>.
+          among them being <a href="http://www.drivethrurpg.com/product/185316/Rusthaven-An-Iron-Wind-Sourcebook-for-Numenera?affiliate_id=201046">Rusthaven</a>.
         </p>
         <p>
           If you care to follow along with the things I'm doing, you can <a href="http://darkliquid.co.uk">read my blog</a> or <a href="http://twitter.com/darkliquid">follow me on twitter</a>.
